e1e93a84939e5fb4d47b6def350755b8e7245eb1
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-09-05 16:51:15 +1200

    Added EJS dependency

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //Require libraries
 require('dotenv').config();
 const express = require('express');
+const ejs = require('ejs');
 
 //Express configuration
 const app = express();
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 	res.send('Hello World');
