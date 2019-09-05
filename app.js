26f23c975b3232e79346e79aa38d0804f521af0b
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-09-05 21:13:28 +1200

    Change username input to text for quicker testing

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 app.use(
 	bodyParser.urlencoded({
 		extended: true
 	})
 );
-app.set('view engine', 'ejs');
 
 //Global Constants
 const serverPort = process.env.PORT || 3000;
@@ -37,7 +37,8 @@ app
 	})
 	.post((req, res) => {
 		console.log(req.body);
-		res.redirect('/login');
+		// res.redirect('/login');
+		res.send(req.body);
 	});
 
 //Open listening port for server requests
