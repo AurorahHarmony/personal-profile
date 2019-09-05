f753e6415d6efb61c81d1236ed3daf5facd6b756
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-09-05 16:48:14 +1200

    Add Login GET and POST route

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.get('/', (req, res) => {
 	res.send('Hello World');
 });
 
+app
+	.route('/login')
+	.get((req, res) => {
+		res.render('login');
+	})
+	.post((req, res) => {
+		res.redirect('/login');
+	});
+
 //Open listening port for server requests
 app.listen(process.env.PORT, err => {
 	console.log(`Server has started on port ${process.env.PORT}`);
