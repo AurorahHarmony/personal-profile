33c6639320ce0b87a2fdd31125a0a20534016e68
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-09-05 14:10:03 +1200

    Add comments

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+//Require libraries
 require('dotenv').config();
 const express = require('express');
 
+//Express configuration
 const app = express();
 
 app.get('/', (req, res) => {
 	res.send('Hello World');
 });
 
+//Open listening port for server requests
 app.listen(process.env.PORT, err => {
 	console.log(`Server has started on port ${process.env.PORT}`);
 });
