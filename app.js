3d04f620d78a441153a027dff07ffa9a787b2c3f
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-09-05 14:03:20 +1200

    Move server variables to environment variables

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
@@ -6,6 +7,6 @@ app.get('/', (req, res) => {
 	res.send('Hello World');
 });
 
-app.listen(3000, err => {
-	console.log('Server has started on port 3000');
+app.listen(process.env.PORT, err => {
+	console.log(`Server has started on port ${process.env.PORT}`);
 });
