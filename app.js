42d4940e85ff9cc8b54f00eb76b066850fedb40f
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-09-06 22:00:42 +1200

    Add bycrypt

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const session = require('express-session');
 const flash = require('req-flash');
 const passport = require('passport');
