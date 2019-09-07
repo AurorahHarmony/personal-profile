b22030922bfb3bd7c65a5aeb702d31ad40d7ce14
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-09-07 22:46:08 +1200

    Change comment to be more descriptive

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const route = express.Router();
 
-//Mongoose Models
+//Import Mongoose Models
 const User = require('../models/user');
 
 const websiteName = 'Profiler:';
