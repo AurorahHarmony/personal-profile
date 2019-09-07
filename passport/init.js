71500be6382dad23aa984eac1b38511da20b298c
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-09-07 22:45:31 +1200

    remove unnecessary console log

--- passport/init.js
+++ passport/init.js
@@ -4,14 +4,11 @@ const User = require('../models/user');
 
 module.exports = passport => {
 	passport.serializeUser((user, done) => {
-		console.log('serializing user: ');
-		console.log(user);
 		done(null, user._id);
 	});
 
 	passport.deserializeUser((id, done) => {
 		User.findById(id, (err, user) => {
-			console.log('deserializing user:', user);
 			done(err, user);
 		});
 	});
