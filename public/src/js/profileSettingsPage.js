a528e8472b0fc51a21ccf33d28e000fdc27acb45
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-09-10 21:52:29 +1200

    Add escape key detection to close modal

--- public/src/js/profileSettingsPage.js
+++ public/src/js/profileSettingsPage.js
@@ -152,5 +152,12 @@ function updateTriggerHandlers() {
 		}
 	}
 }
-
 updateTriggerHandlers();
+
+//Keypress handler
+document.onkeydown = function(evt) {
+	evt = evt || window.event;
+	if (evt.keyCode == 27) {
+		hideModal();
+	}
+};
